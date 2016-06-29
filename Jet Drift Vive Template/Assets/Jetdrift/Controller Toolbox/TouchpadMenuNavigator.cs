6e6c5e3cb36dc927fe5f719d85ff6a1c54ace31a
Hardy Whitney <htw6174@gmail.com>
2016-06-28 22:08:34 -0400

    test scene for vr menus

---
+++ Jet Drift Vive Template/Assets/Jetdrift/Controller Toolbox/TouchpadMenuNavigator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class TouchpadMenuNavigator : MonoBehaviour {
+
+    public Button testButton;
+    public StandaloneInputModule inputModule;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            testButton.onClick.Invoke();
+        }
+    }
+}
---
+++ Jet Drift Vive Template/Assets/Jetdrift/Controller Toolbox/VRControllerInputModule.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System;
+
+public class VRControllerInputModule : PointerInputModule
+{
+    [SerializeField]
+    private float m_InputInteractionsPerSecond;
+
+    public float inputActionsPerSecond
+    {
+        get { return m_InputInteractionsPerSecond; }
+        set { m_InputInteractionsPerSecond = value; }
+    }
+
+    public override bool ShouldActivateModule()
+    {
+        if (!base.ShouldActivateModule())
+        {
+            return false;
+        }
+
+        bool shouldActivate = false; //Need to set to true when trigger is pressed
+
+        return shouldActivate;
+    }
+
+    public override void Process()
+    {
+        throw new NotImplementedException();
+    }
+}
