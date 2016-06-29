76fce5bd030aa6895179411eed44606badcb7026
Hardy Whitney <htw6174@gmail.com>
2016-06-29 15:13:20 -0400

    Added vr controllable canvas to default scene

--- Jet Drift Vive Template/Assets/Jetdrift/Controller Toolbox/TouchpadMenuNavigator.cs
+++ Jet Drift Vive Template/Assets/Jetdrift/Controller Toolbox/TouchpadMenuNavigator.cs
@@ -5,19 +5,54 @@ using System.Collections;
 
 public class TouchpadMenuNavigator : MonoBehaviour {
 
-    public Button testButton;
-    public StandaloneInputModule inputModule;
+    public GameObject currentSelected; //TODO: automatically assign currentSelected
+    public EventSystem eventSystem; //TODO: automaticall assign eventSystem
+
+    private ControllerInputTracker inputTracker;
 
     void Start()
     {
-
+        inputTracker = transform.GetComponentInParent<ControllerInputTracker>();
     }
 
-    void Update()
+    void OnEnable()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            testButton.onClick.Invoke();
-        }
+        inputTracker.dpadUpTouchedStart += new ControllerInputDelegate(NavigateMenuUp);
+        inputTracker.dpadDownTouchedStart += new ControllerInputDelegate(NavigateMenuDown);
+        inputTracker.triggerPressedDown += new ControllerInputDelegate(MenuSelect);
+    }
+
+    void OnDisable()
+    {
+        inputTracker.dpadUpTouchedStart -= new ControllerInputDelegate(NavigateMenuUp);
+        inputTracker.dpadDownTouchedStart -= new ControllerInputDelegate(NavigateMenuDown);
+        inputTracker.triggerPressedDown -= new ControllerInputDelegate(MenuSelect);
+    }
+
+    private void NavigateMenuUp()
+    {
+        eventSystem.SetSelectedGameObject(currentSelected);
+        AxisEventData axisData = new AxisEventData(eventSystem);
+        axisData.moveDir = MoveDirection.Up;
+        ExecuteEvents.Execute(currentSelected, axisData, ExecuteEvents.moveHandler);
+        currentSelected = eventSystem.currentSelectedGameObject;
+    }
+
+    private void NavigateMenuDown()
+    {
+        eventSystem.SetSelectedGameObject(currentSelected);
+        AxisEventData axisData = new AxisEventData(eventSystem);
+        axisData.moveDir = MoveDirection.Down;
+        ExecuteEvents.Execute(currentSelected, axisData, ExecuteEvents.moveHandler);
+        currentSelected = eventSystem.currentSelectedGameObject;
+    }
+
+    private void MenuSelect()
+    {
+        eventSystem.SetSelectedGameObject(currentSelected);
+        BaseEventData baseData = new BaseEventData(eventSystem);
+        currentSelected = eventSystem.currentSelectedGameObject;
+        baseData.selectedObject = currentSelected;
+        ExecuteEvents.Execute(currentSelected, baseData, ExecuteEvents.submitHandler);
     }
 }
